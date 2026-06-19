6f4f17c6381f5e532b9ecd6750a3aa914b47ce10
rhit-wighths <wighths@rose-hulman.edu>
2026-06-19 16:30:35 -0500

    Updates

---
+++ _next/static/T02TtTLHYcgjPSGv9MJLp/_buildManifest.js
@@ -0,0 +1,16 @@
+self.__BUILD_MANIFEST = {
+  "__rewrites": {
+    "afterFiles": [],
+    "beforeFiles": [
+      {
+        "source": "/harrisonwight.portfolio/portfolio/public/_next/:path+",
+        "destination": "/harrisonwight.portfolio/portfolio/app/_next/:path+"
+      }
+    ],
+    "fallback": []
+  },
+  "sortedPages": [
+    "/_app",
+    "/_error"
+  ]
+};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
