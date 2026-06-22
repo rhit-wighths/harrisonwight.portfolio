1387644d52ad9f3c1793bdabf17cd1b4121c7b45
rhit-wighths <wighths@rose-hulman.edu>
2026-06-22 17:02:42 -0500

    fixed folders

--- next.config.ts
+++ next.config.ts
@@ -5,8 +5,8 @@ const nextConfig: NextConfig = {
   images: {
     unoptimized: true,
   },
-  basePath: process.env.NODE_ENV === "production" ? "/portfolio" : "",
-  assetPrefix: process.env.NODE_ENV === "production" ? "/portfolio/" : "",
+  basePath: "/harrisonwight.portfolio/portfolio",
+  assetPrefix: "/harrisonwight.portfolio/portfolio/public",
 };
 
 export default nextConfig;
