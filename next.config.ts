dff84c24719ade4dea70e1d9819994bcd7f02902
rhit-wighths <wighths@rose-hulman.edu>
2026-06-23 07:49:30 -0500

    Testing Deployment configuration

--- next.config.ts
+++ next.config.ts
@@ -2,11 +2,6 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: "export",
-  images: {
-    unoptimized: true,
-  },
-  basePath: "/harrisonwight.portfolio/portfolio",
-  assetPrefix: "/harrisonwight.portfolio/portfolio/public",
 };
 
 export default nextConfig;
